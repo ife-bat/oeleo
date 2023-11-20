d702e030a7ecda4264207c8055b5427db4e56773
jepegit <jepe@ife.no>
2023-11-20 12:24:19 +0100

    bump to v 0.4.13

--- noxfile.py
+++ noxfile.py
@@ -11,20 +11,34 @@ dotenv.load_dotenv()
 @nox.session
 def pack(session):
 
-    session.install("oeleo")
-
     out = session.run("poetry", "version", silent=True, external=True)
     version = out.strip().replace("oeleo ", "")
     print(f"version: {version}")
     folder_name_base = out.strip().replace(" ", "_").replace(".", "_")
+    print(f"folder_name_base: {folder_name_base}")
+    wheel_name = f"oeleo-{version}-py3-none-any.whl"
 
     print(" creating frozen requirements file ".center(80, "-"))
-    requirements_file = open(r"oeleo-bins\oeleo-requirements.txt", "w")
-    session.run("pip", "freeze", silent=False, stdout=requirements_file)
-    requirements_file.close()
+    session.run(
+        "poetry",
+        "export",
+        "-f",
+        "requirements.txt",
+        "--without-hashes",
+        "--output",
+        r"oeleo-bins\oeleo-requirements.txt",
+        external=True,
+    )
     print("---------------------")
-
     print(" downloading packages for win32 ".center(80, "-"))
+
+    session.run(r"pip", "download", "--platform", "win32", "--no-deps", f"oeleo=={version}", "-d", r"oeleo-bins")
+    win_32_file = pathlib.Path(wheel_name).name.replace("-any.whl", "-any-win32.whl")
+
+    with session.chdir(r"oeleo-bins"):
+        print(f"win_32_file: {win_32_file}")
+        shutil.copy(wheel_name, win_32_file)
+
     with session.chdir(r"oeleo-bins"):
         session.run(
             "pip",
@@ -37,18 +51,12 @@ def pack(session):
             "-d",
             f"{folder_name_base}-win32",
         )
-        oeleo_file = (
-            pathlib.Path(f"{folder_name_base}-win32")
-            .resolve()
-            .glob(f"oeleo-*-py3-none-any.whl")
-        )
-        f = next(oeleo_file)
-        win_32_file = f.name.replace("-any.whl", "any-win32.whl")
-        shutil.copy(f, win_32_file)
 
     print("-------ok-win-32---------")
 
     print(" downloading packages for win64 ".center(80, "-"))
+    session.run(r"pip", "download", "--no-deps", f"oeleo=={version}", "-d", r"oeleo-bins")
+
     with session.chdir(r"oeleo-bins"):
         session.run(
             "pip",
@@ -59,14 +67,6 @@ def pack(session):
             "-d",
             f"{folder_name_base}",
         )
-        oeleo_file = (
-            pathlib.Path(f"{folder_name_base}")
-            .resolve()
-            .glob(f"oeleo-*-py3-none-any.whl")
-        )
-        f = next(oeleo_file)
-        shutil.copy(f, f.name)
-
     print("--------OK-----------")
     print()
 
@@ -97,7 +97,7 @@ def pack(session):
             )
             session.run(
                 "scp",
-                f.name,
+                wheel_name,
                 f"{server_name}:{server_folder}",
                 external=True,
             )
@@ -111,4 +111,4 @@ def pack(session):
     print("To install on win32, use:")
     print(f"pip install {win_32_file} --no-index --find-links {folder_name_base}-win32")
     print("To install on win, use:")
-    print(f"pip install {f.name} --no-index --find-links {folder_name_base}")
+    print(f"pip install {wheel_name} --no-index --find-links {folder_name_base}")
